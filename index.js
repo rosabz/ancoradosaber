b60ed425d99abf8070f54b8dd748030c38b26a68
Seu Nome <seuemail@exemplo.com>
2026-06-15 19:02:20 -0300

    Atualiza documentos e setup da etapa 2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log('Projeto iniciado');
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ mensagem: "API Âncora do Saber funcionando!" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
